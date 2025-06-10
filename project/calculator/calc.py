2d4e4e40fbc570f1da45d81877c5be1457c58c79
Shulin Gao <sleangao@gmail.com>
2025-06-10 15:16:14 +0200

    test pr from other branch

---
+++ project/calculator/calc.py
@@ -0,0 +1,19 @@
+# Define add, subtract, multiply, and divide functions
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a / b
+
+## change from other branch
